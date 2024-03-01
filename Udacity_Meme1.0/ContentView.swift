b29114fc3caba02642f6247f589026fed95e0fc7
VuCuongHD96 <xuancuonghd1102@gmail.com>
2024-03-01 10:22:17 +0700

    Initial Commit

---
+++ Udacity_Meme1.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Udacity_Meme1.0
+//
+//  Created by Work on 01/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Udacity_Meme1.0/Udacity_Meme1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Udacity_Meme1_0App.swift
+//  Udacity_Meme1.0
+//
+//  Created by Work on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Udacity_Meme1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
